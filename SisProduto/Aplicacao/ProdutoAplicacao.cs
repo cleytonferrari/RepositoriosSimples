faa47e29df19d6b2d1aede3e2a90128b9a2a4d6f
Cleyton Ferrari <cleyton@w7br.com>
2012-12-21 23:02:11 -0400

    Adicionado a camada de aplicação do produto

---
+++ SisProduto/Aplicacao/ProdutoAplicacao.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Dominio;
+using Repositorio.SQLServer;
+
+namespace Aplicacao
+{
+    public class ProdutoAplicacao
+    {
+        private readonly ProdutoRepositorio produtoRepositorio;
+
+        public ProdutoAplicacao()
+        {
+            produtoRepositorio = new ProdutoRepositorio();
+        }
+
+        public List<Produto> ListarProdutoPorNome(string nome)
+        {
+            return produtoRepositorio.ListarTodos().Where(x => x.Nome.ToLower().Contains(nome.ToLower())).ToList();
+        }
+
+        public List<Produto> ListarProdutoPorCategoria(string categoria)
+        {
+            return produtoRepositorio.ListarTodos().Where(x => x.Categoria.ToLower().Contains(categoria.ToLower())).ToList();
+        }
+
+        public bool InserirProduto(Produto produto)
+        {
+            //fazer a validação do produto aqui
+            return produtoRepositorio.Inserir(produto);
+        }
+
+        public bool AlterarProduto(Produto produto)
+        {
+            //fazer a validação do produto aqui
+            return produtoRepositorio.Alterar(produto);
+        }
+
+        public bool ExcluirProuto(int id)
+        {
+            //validar se existe antes de excluir
+            return produtoRepositorio.Excluir(id);
+        }
+    }
+}
--- SisProduto/UI.Dos/Program.cs
+++ SisProduto/UI.Dos/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Aplicacao;
 using Dominio;
 using Repositorio.SQLServer;
 
@@ -12,8 +13,8 @@ namespace UI.Dos
     {
         static void Main(string[] args)
         {
-            var produtoRepositorio = new ProdutoRepositorio();
-            
+            var produtoAplicacao = new ProdutoAplicacao();
+
 
             var produtoAlterar = new Produto()
                               {
@@ -21,24 +22,24 @@ namespace UI.Dos
                                   Categoria = "Ferro",
                                   Nome = "Arruela",
                                   Saldo = 233,
-                                  ValorUnitario =  10
+                                  ValorUnitario = 10
                               };
-            produtoRepositorio.Alterar(produtoAlterar);
-            
-            
+            produtoAplicacao.AlterarProduto(produtoAlterar);
+
+
 
             var produtoInserir = new Produto()
             {
                 ProdutoId = 1,
-                Categoria = "ARROZ",
-                Nome = "COMIDA",
+                Categoria = "CARNE",
+                Nome = "CARNE MOIDA",
                 Saldo = 4,
                 ValorUnitario = 10
             };
 
-            produtoRepositorio.Inserir(produtoInserir);
+            produtoAplicacao.InserirProduto(produtoInserir);
 
-            var produtos = produtoRepositorio.ListarTodos();
+            var produtos = produtoAplicacao.ListarProdutoPorNome("CAR");
             foreach (var produto in produtos)
             {
                 Console.WriteLine("{0} - {1} - {2} - R$ {3} - {4}", produto.ProdutoId, produto.Nome, produto.Categoria, produto.ValorUnitario, produto.Saldo);
