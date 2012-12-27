af52413a620dabd4d2009800fd46c69869738028
Cleyton Ferrari <cleyton@w7br.com>
2012-12-27 11:04:26 -0400

    Adicionado uma UI para WEB;

---
+++ SisProduto/UI.Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Sistema de cadastro de produtos utilizando ASP.Net MVC 4 e SQL Server.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Aprendendo a programar com Cleyton Ferrari.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Sua area de contato.";
+
+            return View();
+        }
+    }
+}
